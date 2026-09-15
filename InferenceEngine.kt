744d9242e6befa6ee7b743dabfc6ae03369cc5d6
nightlais22 <nightlais22@gmail.com>
2026-09-15 09:53:37 +0300

    Delete InferenceEngine.kt

--- InferenceEngine.kt
+++
@@ -1,9 +0,0 @@
-package com.example.localllm.llm
-
-/** Inference-слой: реализация отвечает за загрузку модели и генерацию. */
-interface InferenceEngine {
-    suspend fun loadModel(path: String): Result<Unit>
-    suspend fun generate(prompt: String, onToken: (String) -> Unit): Result<String>
-    fun isLoaded(): Boolean
-    fun unload()
-}
